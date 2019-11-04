abbb8ae82a78825bf69f0d7a9e51f848ce1e3fdf
shanerbo <shanerbo@gmail.com>
2019-11-03 21:20:52 -0800

    add category class aka add new table and foreign key

--- main/admin.py
+++ main/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import New
+from .models import New, NewSeries, NewCategory
 from tinymce.widgets import TinyMCE
 from django.db import models
 
@@ -9,7 +9,9 @@ from django.db import models
 class NewAdmin(admin.ModelAdmin):
     fieldsets = [
         ("Title/date", {"fields": ["new_title", "new_publish"]}),
-        ("Content", {"fields": ["new_content"]})
+        ("URL", {"fields": ["new_slug"]}),
+        ("Series", {"fields": ["new_series"]}),
+        ("Content", {"fields": ["new_content"]}),
     ]
 
     formfield_overrides = {
@@ -18,3 +20,5 @@ class NewAdmin(admin.ModelAdmin):
 
 
 admin.site.register(New, NewAdmin)
+admin.site.register(NewSeries)
+admin.site.register(NewCategory)
--- main/models.py
+++ main/models.py
@@ -6,10 +6,10 @@ from datetime import datetime
 class NewCategory(models.Model):
     new_category = models.CharField(max_length=200)
     category_summary = models.CharField(max_length=200)
-    category_slug = models.CharField(max_length=200)
+    category_slug = models.CharField(max_length=200, default=1)
 
     class Meta:
-        verbose_name_plural = "Category"
+        verbose_name_plural = "Categories"
 
     def __str__(self):
         return self.new_category
@@ -17,13 +17,23 @@ class NewCategory(models.Model):
 
 class NewSeries(models.Model):
     new_series = models.CharField(max_length=200)
-    new_category = models.ForeignKey(NewCategory, default=1, verbose_name="Category", on_delete=models.SET_DEFAULT())
+    new_category = models.ForeignKey(NewCategory, default=1, verbose_name="Category", on_delete=models.SET_DEFAULT)
+    series_summary = models.CharField(max_length=200)
+
+    class Meta:
+        verbose_name_plural = "Series"
+
+    def __str__(self):
+        return self.new_series
 
 
 class New(models.Model):
     new_title = models.CharField(max_length=200)
     new_content = models.TextField()
     new_publish = models.DateTimeField("date published", default=datetime.now())
+
+    new_series = models.ForeignKey(NewSeries, default=1, verbose_name="Series", on_delete=models.SET_DEFAULT)
+    new_slug = models.CharField(max_length=200, default=1)
 
     def __str__(self):
         return self.new_title

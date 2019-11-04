9bfe46a3c3611c8943f1060481df24365e0f852b
erbo <erbo_shan@sfu.ca>
2019-11-04 15:46:01 -0800

    add mobile nav bar change text color

--- main/models.py
+++ main/models.py
@@ -1,12 +1,16 @@
 from django.db import models
 from datetime import datetime
 
+default_category_url = "https://storage.googleapis.com/mysite-cardview-img/categories_default_img.jpeg"
+default_series_url = "https://storage.googleapis.com/mysite-cardview-img/series_default_img.jpeg"
+default_new_url = "https://storage.googleapis.com/mysite-cardview-img/new_default_img.jpeg"
 
 # Create your models here.
 class NewCategory(models.Model):
     new_category = models.CharField(max_length=200)
     category_summary = models.CharField(max_length=200)
     category_slug = models.CharField(max_length=200, default=1)
+    category_url = models.CharField(max_length=200, default=default_category_url)
 
     class Meta:
         verbose_name_plural = "Categories"
@@ -19,6 +23,7 @@ class NewSeries(models.Model):
     new_series = models.CharField(max_length=200)
     new_category = models.ForeignKey(NewCategory, default=1, verbose_name="Category", on_delete=models.SET_DEFAULT)
     series_summary = models.CharField(max_length=200)
+    series_url = models.CharField(max_length=200, default=default_series_url)
 
 
     class Meta:
@@ -36,6 +41,8 @@ class New(models.Model):
     new_series = models.ForeignKey(NewSeries, default=1, verbose_name="Series", on_delete=models.SET_DEFAULT)
 
     new_slug = models.CharField(max_length=200, default=1)
+
+    new_url = models.CharField(max_length=200, default=default_new_url)
 
     def __str__(self):
         return self.new_title

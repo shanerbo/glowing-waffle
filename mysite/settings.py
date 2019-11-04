1cc8a8ef17e08d498810f5ebe8ec24718a2199ea
erbo <erbo_shan@sfu.ca>
2019-11-04 11:14:21 -0800

    code format

--- mysite/settings.py
+++ mysite/settings.py
@@ -14,6 +14,9 @@ import os
 
 # Mode
 
+MODE = 'GCP'
+
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -81,36 +84,39 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'mysite',
-#         'USER': 'admin',
-#         'PASSWORD': 'admin',
-#         'HOST': '34.82.191.27',
-#         'PORT': '5432',
-#     }
-# }
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'mysite',
-        'USER': 'Unchained_Erbo',
-        'PASSWORD': 'p',
-        'HOST': 'localhost',
-        'PORT': '5432',
+if MODE == 'remote':
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'mysite',
+            'USER': 'admin',
+            'PASSWORD': 'admin',
+            'HOST': '34.82.191.27',
+            'PORT': '5432',
+        }
     }
-}
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'mysite',
-#         'USER': 'admin',
-#         'PASSWORD': 'admin',
-#         'HOST': '/cloudsql/erbo-6496:us-west1:mysite',
-#         'PORT': '5432',
-#     }
-# }
+elif MODE == 'local':
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'mysite',
+            'USER': 'Unchained_Erbo',
+            'PASSWORD': 'p',
+            'HOST': 'localhost',
+            'PORT': '5432',
+        }
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'mysite',
+            'USER': 'admin',
+            'PASSWORD': 'admin',
+            'HOST': '/cloudsql/erbo-6496:us-west1:mysite',
+            'PORT': '5432',
+        }
+    }
 
 # Tinymce editor
 # https://www.tiny.cloud/docs/configure/

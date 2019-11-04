8d4ee69e81b025ec7be4ad7a926dfae2703a859f
shanerbo <shanerbo@gmail.com>
2019-11-03 19:45:40 -0800

    register page and error msg toast

--- main/urls.py
+++ main/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.urls import path
 from . import views
+
 app_name = "main"
+
 urlpatterns = [
     path("", views.homepage, name="homepage"),
+    path("register/", views.register, name="register"),
+
 ]

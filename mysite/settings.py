a64561b7a0993044948cd8a91093af2d2591fe60
erbo <erbo_shan@sfu.ca>
2019-11-04 03:38:02 -0800

    modified yaml file create requirements.txt where indicates dependencies

--- mysite/settings.py
+++ mysite/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 
+# Mode
+mode = 'gcp'
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -74,12 +77,41 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if mode == 'remote':
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'mysite',
+            'USER': 'admin',
+            'PASSWORD': 'admin',
+            'HOST': '34.82.191.27',
+            'PORT': '5432',
+        }
     }
-}
+elif mode=='local':
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'mysite',
+            'USER': 'Unchained_Erbo',
+            'PASSWORD': 'p',
+            'HOST': 'localhost',
+            'PORT': '5432',
+        }
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'mysite',
+            'USER': 'admin',
+            'PASSWORD': 'admin',
+            'HOST': 'erbo-6496:us-west1:mysite',
+            'PORT': '5432',
+        }
+    }
+
+
 
 # Tinymce editor
 # https://www.tiny.cloud/docs/configure/
